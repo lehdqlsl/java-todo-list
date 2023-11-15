9139dc65ce727fed21243ba1855f596e35959793
jinmlee <jinmlee210@naver.com>
2023-11-15 21:55:44 +0900

    feat: 선택한 옵션에 따라 이동하는 기능구현

---
+++ src/main/java/org/homework/model/ToDoList.java
@@ -0,0 +1,20 @@
+package org.homework.model;
+
+import java.util.HashMap;
+
+public class ToDoList {
+    private HashMap<Integer, String> toDoMap = new HashMap<>();
+    private int idCount = 1;
+
+    public void addList(){
+
+    }
+
+    public void deleteList(){
+
+    }
+
+    public void viewList(){
+
+    }
+}
