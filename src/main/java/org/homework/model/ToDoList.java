63bbe841d8e3540b1216480f5736083596be3d90
jinmlee <jinmlee210@naver.com>
2023-11-15 23:15:27 +0900

    feat: 할일 목록을 조회하고 출력하는 기능 구현

--- src/main/java/org/homework/model/ToDoList.java
+++ src/main/java/org/homework/model/ToDoList.java
@@ -6,26 +6,33 @@ public class ToDoList {
     private HashMap<Integer, String> toDoMap;
     private int id;
 
-    public ToDoList(){
+    public ToDoList() {
         this.toDoMap = new HashMap<>();
         this.id = 1;
     }
 
-    public int addList(String addInput){
+    public HashMap<Integer, String> getToDoMap() {
+        return toDoMap;
+    }
+
+    public int addList(String addInput) {
         toDoMap.put(id, addInput);
         id++;
         return id - 1;
     }
 
-    public int deleteList(int deleteNumber){
-        if(toDoMap.containsKey(deleteNumber)){
+    public int deleteList(int deleteNumber) {
+        if (toDoMap.containsKey(deleteNumber)) {
             toDoMap.remove(deleteNumber);
             return deleteNumber;
         }
         return -1;
     }
 
-    public void viewList(){
-
+    public int viewList(int viewNumber) {
+        if (toDoMap.containsKey(viewNumber)) {
+            return viewNumber;
+        }
+        return -1;
     }
 }
