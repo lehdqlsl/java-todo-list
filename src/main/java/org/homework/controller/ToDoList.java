10439f7052cc1e7f3f11d53847d9307c9e8e6079
jinmlee <jinmlee210@naver.com>
2023-11-15 21:06:48 +0900

    feat: 옵션 선택 번호에 대한 예외처리 기능 구현

---
+++ src/main/java/org/homework/controller/ToDoList.java
@@ -0,0 +1,10 @@
+package org.homework.controller;
+
+import org.homework.view.InputView;
+
+public class ToDoList {
+    private final InputView inputView = new InputView();
+    public void run() {
+        inputView.selectOption();
+    }
+}
--- src/main/java/org/homework/view/InputView.java
+++ src/main/java/org/homework/view/InputView.java
@@ -1,16 +1,18 @@
 package org.homework.view;
 
+import org.homework.util.InputCheck;
+
 import java.util.Scanner;
 
 public class InputView {
+    private final InputCheck inputCheck = new InputCheck();
     Scanner scanner = new Scanner(System.in);
 
-    public void selectOption() {
+    public int selectOption() {
         while (true) {
             try {
                 System.out.println("옵션을 선택하세요: 1.추가, 2.삭제, 3.조회, 4.종료");
-                String input =  scanner.nextLine();
-                return;
+                return inputCheck.checkSelectNumber(scanner.nextLine());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
